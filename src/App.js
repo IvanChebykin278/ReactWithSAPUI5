ca67371dba3f17ec51759966fcd4ddef14bac282
Chebykin Ivan <chebykin.i.v@gmail.com>
2019-09-15 23:44:34 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,39 @@
+/*eslint no-undef: "off"*/
+/*eslint no-restricted-globals: "off"*/
+/*eslint no-unused-expressions: "off"*/
+//'https://services.odata.org/V2/OData/OData.svc/'
+import React, { Component } from 'react';
+import rootReducer from "./store/reducers";
+import {createStore, applyMiddleware, compose} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from "react-redux";
+import Page from "./components/Page";
+
+const initialState = {};
+
+const middleware = [thunk];
+
+const store = createStore(
+  rootReducer,
+  initialState,
+  compose(
+    applyMiddleware(...middleware),
+        window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+export default class App extends Component {
+
+  render() {
+    return (
+      <Provider store={store}>
+        <div>
+          hello world
+          <Page />
+        </div>
+      </Provider>
+    )
+  }
+
+}
+
